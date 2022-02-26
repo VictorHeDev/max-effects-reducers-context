0dcab4e5211d64066ad73c4054c557182000fed6
Woodstock <victorhe97@gmail.com>
2022-02-26 11:31:25 -0500

    Add empty invoked onLogout method to the auth-context.js file

--- src/components/MainHeader/Navigation.js
+++ src/components/MainHeader/Navigation.js
@@ -6,7 +6,7 @@ import classes from './Navigation.module.css';
 // context
 import AuthContext from '../../store/auth-context';
 
-const Navigation = (props) => {
+const Navigation = () => {
   const ctx = useContext(AuthContext);
 
   return (
@@ -24,7 +24,7 @@ const Navigation = (props) => {
         )}
         {ctx.isLoggedIn && (
           <li>
-            <button onClick={props.onLogout}>Logout</button>
+            <button onClick={ctx.onLogout}>Logout</button>
           </li>
         )}
       </ul>

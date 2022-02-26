53ad058902213e5bfccd596af13c5038652841b6
Woodstock <victorhe97@gmail.com>
2022-02-25 20:11:14 -0500

    Remove isAuthenticated prop to the main header

--- src/App.js
+++ src/App.js
@@ -33,8 +33,12 @@ function App() {
 
   return (
     <React.Fragment>
-      <AuthContext.Provider>
-        <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
+      <AuthContext.Provider
+        value={{
+          isLoggedIn: isLoggedIn,
+        }}
+      >
+        <MainHeader onLogout={logoutHandler} />
         <main>
           {!isLoggedIn && <Login onLogin={loginHandler} />}
           {isLoggedIn && <Home onLogout={logoutHandler} />}
